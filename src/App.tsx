8f284df655397d975735bff5554f99299aa0f677
dongyuekai <457545648@qq.com>
2024-02-01 15:20:20 +0800

    feat：test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   useEffect(() => {
-    const eventSource = new EventSource('http://localhost:3000/stream')
+    const eventSource = new EventSource('http://localhost:3000/stream2')
     eventSource.onmessage = ({data}) => {
-      console.log('New message', JSON.parse(data));
+      console.log('New message----', JSON.parse(data));
     }
   },[]);
   return (
