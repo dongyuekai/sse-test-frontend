0dbe9214f78e2a258871b73b58e65b0cf67ae812
dongyuekai <457545648@qq.com>
2024-02-01 14:41:27 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    const eventSource = new EventSource('http://localhost:3000/stream')
+    eventSource.onmessage = ({data}) => {
+      console.log('New message', JSON.parse(data));
+    }
+  },[]);
+  return (
+    <div>hello</div>
+  );
+}
+
+export default App;
